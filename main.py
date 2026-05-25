a59a0b796419a05119e9dcfe326754d65bf8c47f
jhonny5 <jhorguma@gmail.com>
2026-05-25 12:41:32 -0500

    Agregue un cliente a la lista de clientes

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-lista_clientes = [{"nombre":"Juan","edad": 15,"descripcion": "N.A"},{"nombre":"Ana","edad": 18,"descripcion": "N.A"}]
+lista_clientes = [
+    {"nombre": "Juan", "edad": 15, "descripcion": "N.A"},
+    {"nombre": "Ana", "edad": 18, "descripcion": "N.A"},
+    {"nombre": "Pepe", "edad": 16, "descripcion": "Nuevo cliente"},
+]
+
 
 @app.get("/clientes")
 def listar_clientes():
-    return{"clientes":lista_clientes}
+    return {"clientes": lista_clientes}
+
 
 @app.post("/clientes")
 def crear_clientes():
-    return{"mensaje":"Cliente creado"}
+    return {"mensaje": "Cliente creado"}
+
 
 @app.put("/clientes")
 def editar_clientes():
-    return{"mensaje":"Cliente Editado"}
+    return {"mensaje": "Cliente Editado"}
+
 
 @app.delete("/clientes")
 def eliminar_clientes():
-    return{"mensaje":"Cliente eliminado"}
-
+    return {"mensaje": "Cliente eliminado"}
