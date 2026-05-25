8943085dfa9ade11526f3221d378f26c8658e911
jhonny5 <jhorguma@gmail.com>
2026-05-25 13:38:42 -0500

    Creamos el modelo clientes, y borramo la lista clientes estatica, y funciona el listar y adicionar clientes, parcialmente

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-lista_clientes = [
-    {"nombre": "Juan", "edad": 15, "descripcion": "N.A"},
-    {"nombre": "Ana", "edad": 18, "descripcion": "N.A"},
-    {"nombre": "Pepe", "edad": 16, "descripcion": "Nuevo cliente"},
-]
+lista_clientes = []
 
 
+# Crear clase llamado MODELO
+class Cliente(BaseModel):
+    id: int
+    nombre: str
+    edad: int
+    descripcion: str
+
+
+# endpoint
 @app.get("/clientes")
 def listar_clientes():
     return {"clientes": lista_clientes}
 
 
+# endpoint
 @app.post("/clientes")
-def crear_clientes():
+def crear_clientes(datos_cliente: Cliente):
+    lista_clientes.append(datos_cliente)
     return {"mensaje": "Cliente creado"}
 
 
+# endpoint
 @app.put("/clientes")
 def editar_clientes():
     return {"mensaje": "Cliente Editado"}
 
 
+# endpoint
 @app.delete("/clientes")
 def eliminar_clientes():
     return {"mensaje": "Cliente eliminado"}
