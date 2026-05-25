5f06e50665603c1f8ff8506a498e44ce93b56068
jhonny5 <jhorguma@gmail.com>
2026-05-25 12:28:49 -0500

    primer commit, crud con mensajes

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+lista_clientes = [{"nombre":"Juan","edad": 15,"descripcion": "N.A"},{"nombre":"Ana","edad": 18,"descripcion": "N.A"}]
+
+@app.get("/clientes")
+def listar_clientes():
+    return{"clientes":lista_clientes}
+
+@app.post("/clientes")
+def crear_clientes():
+    return{"mensaje":"Cliente creado"}
+
+@app.put("/clientes")
+def editar_clientes():
+    return{"mensaje":"Cliente Editado"}
+
+@app.delete("/clientes")
+def eliminar_clientes():
+    return{"mensaje":"Cliente eliminado"}
+
